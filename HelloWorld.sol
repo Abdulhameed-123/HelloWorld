0683a028bd23f4a8609ae2beb3266f6370914089
"Abdulhameed-123" <"yusufhameed125@gmail.com">
2022-03-10 11:35:31 +0100

    first commit

---
+++ HelloWorld.sol
@@ -0,0 +1,20 @@
+//Spdx-Licence-identificer: MIT;
+
+pragma solidity ^0.8.7;
+
+contract HelloWorld {
+   string public message;
+   address public owner;
+
+   constructor(string memory _message) public {
+       owner = msg.sender;
+   }
+
+   function store (string memory _message) public {
+       message = message;
+   }
+   function retrieve () public view returns (string memory _message) {
+    return message;
+}
+
+}
